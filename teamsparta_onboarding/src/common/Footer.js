f37a9bdadac5e804751f54daa5405227bfe4e0f4
이한빈 <kx1302@naver.com>
2024-09-11 14:46:32 +0900

    feat: Footer-section2 구현

--- teamsparta_onboarding/src/common/Footer.js
+++ teamsparta_onboarding/src/common/Footer.js
@@ -3,7 +3,55 @@
 export default function Footer() {
   return(
     <>
-      <h1>here is Footer</h1>
+      <footer className="w-full flex justify-center border-t border-[#E4EBF0] bg-white px-[16px] py-[34px] break-keep">
+        <footer className="w-full max-w-[1152px] min-w-[328px] mx-auto flex flex-col gap-[24px]">
+          <section className="w-full flex flex-col gap-[24px]">
+            section1
+          </section>
+          <div className="block w-full min-h-[1px] bg-[#E4EBF0]"></div>   {/* footer 중간 구분선 */}
+          <section className="w-full flex flex-col gap-[16px]">
+            <div className="flex justify-between">
+              <section className="flex items-center gap-[16px]">
+                <a data-bold="true" target="_blank" className="font-pretendard font-normal text-[13px] leading-[20px] text-[#5F666B] font-bold p-[4px] cursor-pointer">개인정보처리방침</a>
+                <a data-bold="false" target="_blank" className="font-pretendard font-normal text-[13px] leading-[20px] text-[#81898F] p-[4px] cursor-pointer">서비스 이용약관</a>
+                <a data-bold="false" target="_blank" className="font-pretendard font-normal text-[13px] leading-[20px] text-[#81898F] p-[4px] cursor-pointer">고객센터</a>
+              </section>
+              <section className="flex-row flex gap-[32px]">
+                <div className="flex items-center gap-[6px]">
+                  <a>
+                    <img className ="w-[32px] h-[32px] text-transparent cursor-pointer" src = "/footer/kakaotalk.webp"></img>
+                  </a>
+                  <a>
+                    <img className ="w-[32px] h-[32px] text-transparent cursor-pointer" src = "/footer/blog.webp"></img>
+                  </a>
+                  <a>
+                    <img className ="w-[32px] h-[33.33px] text-transparent cursor-pointer" src = "/footer/instagram.svg"></img>
+                  </a>
+                  <a>
+                    <img className ="w-[32px] h-[33.33px] text-transparent cursor-pointer" src = "/footer/youtube.webp"></img>
+                  </a>
+                </div>
+                <div className="flex items-center gap-[12px]">
+                  <img className="w-[36px] h-[36px] text-transparent" src="/footer/winner.webp"></img>
+                  <p className="font-pretendard font-normal font-[400] text-[13px] leading-[20px] text-[#9DA7AE]">
+                    2022-23 올해의 브랜드 대상
+                    <br></br>
+                    코딩교육 부문 2년 연속 1위
+                  </p>
+                </div>
+              </section>
+            </div>
+            <section className="font-pretendard font-medium text-[12px] leading-[18px] text-[#9DA7AE] flex flex-col gap-[4px]">
+              <p>팀스파르타(주) 사업자 정보</p>
+              <p>대표자 : 이범규ㅣ사업자 등록번호 : 783-86-01715ㅣ통신판매업 신고번호 : 2020-서울강남-02300ㅣ평생교육시설 신고번호 : 제 661호
+                <br></br>
+                주소 : 서울특별시 강남구 테헤란로44길 8 12층ㅣ이메일 : contact@teamsparta.coㅣ전화 : 1522-8016
+              </p>
+            </section>
+            <p className="font-pretendard font-medium text-[12px] leading-[18px] text-[#9DA7AE]">Copyright ©2024 TEAMSPARTA. All rights reserved.</p>
+          </section>
+        </footer>
+      </footer>
     </>
   );
 }
--- teamsparta_onboarding/src/common/Navbar.js
+++ teamsparta_onboarding/src/common/Navbar.js
@@ -7,11 +7,11 @@ export default function Navbar() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <nav className="fixed w-full h-auto backdrop-blur-[10px] flex flex-row bg-white transition-all duration-300 ease border-b border-[#e4ebf0] z-[21]">
-      <div className="font-pretendard font-normal text-[16px] leading-[26px] text-[#141617] h-[60px] max-w-[1900px] px-[24px] flex items-center w-[100%] justify-center">
+    <nav className="fixed w-full h-auto backdrop-blur-[10px] flex flex-row bg-white justify-center transition-all duration-300 ease border-b border-[#e4ebf0] z-[21]">
+      <div className="font-pretendard font-normal text-[16px] leading-[26px] text-[#141617] h-[60px] max-w-[1200px] px-[24px] flex items-center w-[100%] justify-between">
         {/* 왼쪽 로고 */}
         <div className="flex items-center gap-[40px]">
-          <img src="/navbar.svg" alt="Sparta Logo" className="cursor-pointer mt-[3px]" />
+          <img src="/navbar.svg" alt="Sparta Logo" className="w-[134px] h-[40px] aspect-[134/40]"/>
         
           {/* 중앙 링크 */}
           <div className="flex space-x-4">
@@ -31,25 +31,30 @@ export default function Navbar() {
         </div>
 
          {/* 오른쪽 고객센터 드롭다운 */}
-         <div className="relative flex justify-between">
-          <div>나의 캠프</div>
-          <div class="mx-3">|</div>
-          <button
-            onClick={() => setDropdownOpen(!dropdownOpen)}
-            className="text-gray-700 hover:text-gray-900 focus:outline-none"
-          >
-            고객센터
-          </button>
-          {dropdownOpen && (
-            <div className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-md py-2">
-              <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
-                자주 묻는 질문
-              </a>
-              <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
-                문의하기
-              </a>
-            </div>
-          )}
+         <div className="flex items-center gap-[8px]">
+          <div className="px-[10px] py-[6px] rounded-[6px] text-[#81898f] relative">나의 캠프</div>
+
+          <div class="px-[10px] py-[6px] rounded-[6px] text-[#81898f] relative">|</div>
+          
+          <div className="relative flex gap-[4px] items-center px-[10px] py-[1px] bg-[#eef3f6] rounded-[6px]">
+            <button
+              onClick={() => setDropdownOpen(!dropdownOpen)}
+              className="relative flex gap-[4px] items-center px-[10px] py-[6px] bg-[#eef3f6] rounded-[6px]"
+            >
+              고객센터
+            </button>
+            {dropdownOpen && (
+              <div className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-md py-2">
+                <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                  자주 묻는 질문
+                </a>
+                <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                  문의하기
+                </a>
+              </div>
+            )}
+          </div>
+
         </div>
       </div>
     </nav>
