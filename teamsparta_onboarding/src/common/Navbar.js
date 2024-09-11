03c9f249c82bffa9d24bb50ad272d77fad06891f
이한빈 <kx1302@naver.com>
2024-09-11 17:07:29 +0900

    Feat: Navbar 구현

--- teamsparta_onboarding/src/common/Navbar.js
+++ teamsparta_onboarding/src/common/Navbar.js
@@ -1,43 +1,56 @@
 "use client"; // 클라이언트 컴포넌트로 지정
 
 import { useState } from "react";
+import { DownArrowIcon } from "./DownArrowIcon";
 
 export default function Navbar() {
   // 드롭다운 메뉴 상태 관리
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <nav className="fixed w-full h-auto backdrop-blur-[10px] flex flex-row bg-white justify-center transition-all duration-300 ease border-b border-[#e4ebf0] z-[21]">
+    <nav className="items-center fixed w-full h-fit backdrop-blur-[10px] flex flex-col justify-center bg-white transition-all duration-300 ease border-b border-[#e4ebf0] z-[21]">
       <div className="font-pretendard font-normal text-[16px] leading-[26px] text-[#141617] h-[60px] max-w-[1200px] px-[24px] flex items-center w-[100%] justify-between">
         {/* 왼쪽 로고 */}
         <div className="flex items-center gap-[40px]">
-          <img src="/navbar.svg" alt="Sparta Logo" className="w-[134px] h-[40px] aspect-[134/40]"/>
+          <a><img src="/Navbar/navbar.svg" alt="Sparta Logo" className="w-[134px] h-[40px] aspect-[134/40] text-transparent cursor-pointer mb-[4px]"></img></a>
         
           {/* 중앙 링크 */}
-          <div className="flex space-x-4">
-            <div className="flex gap-[4px] items-center cursor-pointer">
-              <a href="#" className="relative flex gap-[4px] items-center px-[10px] py-[6px]">부트캠프</a>
-              <div className="w-[20px] h-[20px] fill-none overflow-hidden"></div>
+          <div className="flex gap-[8px]">
+            <div className="relative flex gap-[4px] items-center px-[10px] py-[6px]">
+              <span>부트캠프</span>
+              <DownArrowIcon/>
             </div>
 
-            <div className="py-[6px] px-[10px]">취업 지원</div> 
-            <div className="flex gap-[4px] items-center cursor-pointer">
-              <a href="#" className="relative flex gap-[4px] items-center px-[10px] py-[6px]">후기&블로그</a>
-              <div className="w-[20px] h-[20px] fill-none overflow-hidden"></div>
+            <div className="py-[6px] px-[10px]">취업 지원</div>
+
+            <div className="relative flex gap-[4px] items-center px-[10px] py-[6px]">
+              <span>후기&블로그</span>
+              <DownArrowIcon/>
             </div>
+            
             <div className="py-[6px] px-[10px]">이벤트</div>
             
           </div>
         </div>
 
+
          {/* 오른쪽 고객센터 드롭다운 */}
          <div className="flex items-center gap-[8px]">
-          <div className="px-[10px] py-[6px] rounded-[6px] text-[#81898f] relative">나의 캠프</div>
-
-          <div class="px-[10px] py-[6px] rounded-[6px] text-[#81898f] relative">|</div>
+          <span className="text-[#81898F] cursor-pointer block">
+            <a href="/mypage/apply-status">
+              <div className="px-[10px] py-[6px] rounded-[6px] text-[#81898F] relative">나의 캠프</div>
+            </a>
+          </span>
           
-          <div className="relative flex gap-[4px] items-center px-[10px] py-[1px] bg-[#eef3f6] rounded-[6px]">
-            <button
+          <span className="rounded-[6px] text-[#81898F] relative">|</span>
+          
+          <div className="relative flex gap-[4px] items-center px-[10px] py-[1px] rounded-[6px]">
+            <div className="flex gap-[4px] items-center cursor-pointer">
+              <span id="__next_div44_nav3a_div3a_divec_divec_div71_span71" className="text-[#81898F]">고객센터</span>
+              <DownArrowIcon color="#81898F"></DownArrowIcon>
+            </div>
+          </div>
+            {/* <button
               onClick={() => setDropdownOpen(!dropdownOpen)}
               className="relative flex gap-[4px] items-center px-[10px] py-[6px] bg-[#eef3f6] rounded-[6px]"
             >
@@ -52,10 +65,11 @@ export default function Navbar() {
                   문의하기
                 </a>
               </div>
-            )}
-          </div>
+            )} */}
 
         </div>
+
+        
       </div>
     </nav>
   );
